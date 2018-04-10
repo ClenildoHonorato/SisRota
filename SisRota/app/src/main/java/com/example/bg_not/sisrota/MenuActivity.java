c44a42c0db84bcd652d272937d01dc39fea08cf8
ClenildoHonorato <clenildohonorato@gmail.com>
2018-04-09 22:36:12 -0300

    Novas Telas

--- SisRota/app/src/main/java/com/example/bg_not/sisrota/MenuActivity.java
+++ SisRota/app/src/main/java/com/example/bg_not/sisrota/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.bg_not.sisrota;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+    public void TelaProducao (View viewc){
+        Intent tp = new Intent(MenuActivity.this, TelaProducao.class);
+        startActivity(tp);
     }
 }
---
+++ SisRota/app/src/main/java/com/example/bg_not/sisrota/RelatorioDiario.java
@@ -0,0 +1,13 @@
+package com.example.bg_not.sisrota;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelatorioDiario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relatorio_diario);
+    }
+}
